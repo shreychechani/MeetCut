3d5e87fa6954d37b0054e0b3ae4db7ba0d3411c3
shrey_chechani <shreychechani@gmail.com>
2026-02-28 17:41:46 +0530

    Checking Landing page is updated before pulling priyanshu's code

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -26,7 +26,8 @@ function Landing() {
 
             {/* Hero Section */}
             <div className="max-w-6xl mx-auto px-6 py-32 text-center">
-                <h1 className="text-6xl font-bold mb-6 leading-tight">Turn Long Meetings into
+                <h1 className="text-6xl font-bold mb-6 leading-tight">
+                    Turn Long Meetings into
                     <br />
                     <span className="text-blue-600">Smart Insights</span>
                 </h1>
@@ -49,4 +50,4 @@ function Landing() {
     );
 }
 
-export default Landing;
+export default Landing
