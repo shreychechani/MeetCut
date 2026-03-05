7925f74435e44b0cece460265bff5edabbc8b841
shrey_chechani <shreychechani@gmail.com>
2026-03-05 11:32:52 +0530

    added route to dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
  
