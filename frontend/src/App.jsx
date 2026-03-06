ac052ba1a83cd153d5fb1a7ddc1a08b90f07f463
shrey_chechani <shreychechani@gmail.com>
2026-03-06 09:36:28 +0530

    Added Dashboard import and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
