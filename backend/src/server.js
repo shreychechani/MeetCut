114e7188718817afcac5d973aea32c7401d2cb92
shrey_chechani <shreychechani@gmail.com>
2026-03-09 16:42:05 +0530

    Updated files -shrey

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,14 @@ connectDB()
 
 const app = express()
 
-app.use(cors())
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
